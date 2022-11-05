709b497e57e105af22ddbc8800f9df8122a34ca6
Long Guan <2668084184gn@gmail.com>
2022-11-05 18:16:58 -0400

    Day2

--- src/components/screens/Home.js
+++ src/components/screens/Home.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Favorites from "./Favorites";
 import Feed from "./Feed";
@@ -7,9 +7,33 @@ import Player from "./Player";
 import Trending from "./Trending";
 import "../cssFiles/Home.css";
 import SideBar from "../sidebar/SideBar";
+import Login from "./Login";
+import { setClientToken } from "../../spotify";
 
 const Home = () => {
-  return (
+  const [token, setToken] = useState("");
+
+  useEffect(() => {
+    //标签值
+    const token = window.localStorage.getItem("token");
+    const hash = window.location.hash;
+    window.location.hash = "";
+
+    //if token or hash doesn't exist, get token from the hash and assign it to window.localStorage
+    //
+    if (!token && hash) {
+      const _token = hash.split("&")[0].split("=")[1];
+      window.localStorage.setItem("token", _token);
+      setToken(_token);
+      setClientToken(_token);
+    } else {
+      setToken(token);
+      setClientToken(token);
+    }
+  }, []);
+  return !token ? (
+    <Login />
+  ) : (
     <Router>
       <div className="main-body">
         <SideBar />
--- src/components/sidebar/SideBar.js
+++ src/components/sidebar/SideBar.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./SideBar.css";
 import SideBarButton from "./SideBarButton";
 import { MdFavorite } from "react-icons/md";
@@ -6,15 +6,20 @@ import { FaGripfire, FaPlay } from "react-icons/fa";
 import { FaSignOutAlt } from "react-icons/fa";
 import { IoLibrary } from "react-icons/io5";
 import { MdSpaceDashboard } from "react-icons/md";
+import apiClient from "../../spotify";
 
 const SideBar = () => {
+  const [image, setImage] = useState(
+    "https://media.istockphoto.com/id/639085642/vector/user-sign-icon-person-symbol-human-avatar.jpg?s=612x612&w=0&k=20&c=n4Zr1A7GMVTY8rZztu_OnMQ4_dkLBLRRAcTb0IIbQfY="
+  );
+  useEffect(() => {
+    apiClient.get("me").then((response) => {
+      setImage(response.data.images[0].url);
+    });
+  }, []);
   return (
     <div className="sidebar-container">
-      <img
-        src="https://media.istockphoto.com/id/639085642/vector/user-sign-icon-person-symbol-human-avatar.jpg?s=612x612&w=0&k=20&c=n4Zr1A7GMVTY8rZztu_OnMQ4_dkLBLRRAcTb0IIbQfY="
-        className="profile-img"
-        alt="profile"
-      />
+      <img src={image} className="profile-img" alt="profile" />
       <div className="SideBarButton-Each">
         <SideBarButton title="Feed" to="/feed" icon={<MdSpaceDashboard />} />
         <SideBarButton title="Trending" to="/trending" icon={<FaGripfire />} />
