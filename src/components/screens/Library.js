f4b2af92ce76ef8af25bccbcbe1041a9e6750d71
Long Guan <2668084184gn@gmail.com>
2022-11-08 15:13:23 -0500

    library

--- src/components/screens/Library.js
+++ src/components/screens/Library.js
@@ -1,7 +1,36 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import APIkit from "../../spotify";
+import "../cssFiles/Library.css";
 
 const Library = () => {
-  return <div className="screen-container">Library</div>;
+  const [playlists, setPlaylists] = useState([]);
+
+  useEffect(() => {
+    APIkit.get("me/playlists").then(function (response) {
+      setPlaylists(response.data.items);
+      console.log(response.data.items);
+    });
+  }, []);
+
+  return (
+    <div className="screen-container">
+      <div className="library-body">
+        {playlists.map((playlist, index) => {
+          return (
+            <div className="playlist-card" key={index}>
+              <img
+                src={playlist.images[0].url}
+                className="playlist-image"
+                alt="Playlist-Art"
+              ></img>
+              <p className="playlist-title">{playlist.name}</p>
+              <p className="playlist-subtitle">{playlist.tracks.total} songs</p>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default Library;
