f9e91c79f101369e78440649dfc2737bd0829dc7
Long Guan <2668084184gn@gmail.com>
2022-12-22 02:39:01 -0500

    update

--- src/components/queue&songcard/queue/Queue.js
+++ src/components/queue&songcard/queue/Queue.js
@@ -2,18 +2,17 @@ import React from "react";
 import "../../cssFiles/Queue.css";
 
 const Queue = ({ tracks, setCurrentIndex }) => {
-  console.log(tracks[2]?.track?.name);
+  console.log(tracks);
   return (
     <div className="queue-container flex">
       <div className="queue flex">
         <p className="upNext">Up Next</p>
-      </div>
-      <div className="queue-list">
-        {tracks?.map((item) => {
-          <div>
-            <p>{item?.track?.name}</p>
-          </div>;
-        })}
+
+        <div className="queue-list">
+          {tracks?.map((track, index) => (
+            <div key={index}>{`${index + 1}: ${track?.track?.name}`}</div>
+          ))}
+        </div>
       </div>
     </div>
   );
